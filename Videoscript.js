d68ad905330b337ff39ace94b78ef93e676051b3
Bloomboi <pastelxbloom@gmail.com>
2022-07-24 09:36:06 +0100

    new vids

--- Videoscript.js
+++ Videoscript.js
@@ -1,6 +1,8 @@
 let videoSource = new Array();
 videoSource[0] = '/PBimages/animation/video1.mp4';
 videoSource[1] = '/PBimages/animation/video2.mp4';
+videoSource[2] = '/PBimages/animation/MH test.mp4';
+videoSource[3] = '/PBimages/animation/spin succ.mp4';
 let i = 0; // global
 const videoCount = videoSource.length;
 const element = document.getElementById("videoPlayer");
